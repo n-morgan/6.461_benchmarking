0bb918984ce24da5aea223a80b66179070db7136
Noah Yared <noahy8105@gmail.com>
2025-11-17 20:38:38 -0500

    add support for benchmarking base, react, cot, and scope methods
    Closes #1

--- test_model.py
+++ test_model.py
@@ -3,9 +3,11 @@ import json
 import os
 from openai import OpenAI
 from concurrent.futures import ThreadPoolExecutor
-from typing import Any, Dict
+from typing import Any
 from threading import Lock
 from pathlib import Path
+from collections import defaultdict
+
 
 # Initialize client
 client = OpenAI(
@@ -13,46 +15,86 @@ client = OpenAI(
     api_key="sk",
 )
 
-# Load benchmark dataset (with question, answer, cot_prompt)
-with open("benchmark_dataset.json", "r") as f:
-    final_benchmark = json.load(f)
+
+METHODS = ["base", "react", "cot", "scope"]
+
+def load_datasets() -> tuple[int, dict[str, Any]]:
+    datasets = {}
+    for method in METHODS:
+        with open(f"{method}_benchmark_dataset.json", "r") as f:
+            datasets[method] = json.load(f)
+    # number of prompts to test for each method
+    n_prompts = min(map(len, datasets.values()))
+    return n_prompts, datasets
+
+N_PROMPTS, DATASETS = load_datasets()
+
+
+PROJECT_DIR = Path(__file__).parent
+
+def setup_log_files() -> tuple[dict[str, Path]]:
+    # Setup log directory
+    log_dir = PROJECT_DIR / "logs"
+    log_dir.mkdir(exist_ok=True, parents=True)
+
+    base = "model_responses"
+
+    out_log_files = list(map(lambda method: log_dir / f"{method}_{base}.out", METHODS))
+    err_log_files = list(map(lambda method: log_dir / f"{method}_{base}.err", METHODS))
+
+    # remove log files if they exist (we overwrite response files so dont need to be unlinked)
+    for out, err in zip(out_log_files, err_log_files):
+        try:
+            out.unlink()
+        except FileNotFoundError:
+            pass
+        try:
+            err.unlink()
+        except FileNotFoundError:
+            pass
+
+    return dict(zip(METHODS, out_log_files)), dict(zip(METHODS, err_log_files))
+
+OUT_LOG_FILES, ERR_LOG_FILES = setup_log_files()
+
+
+def setup_locks() -> tuple[Any]:
+    out_locks = {method: Lock() for method in METHODS}
+    err_locks = {method: Lock() for method in METHODS}
+
+    return out_locks, err_locks
+
+OUT_LOCKS, ERR_LOCKS = setup_locks()
+
 
 # Useful constants
 N_RETRIES = 2
 N_CORES = os.cpu_count() or 8
 N_WORKERS = N_CORES * 2
-N_PROMPTS = len(final_benchmark)
 
-PROJECT_DIR = Path(__file__).parent
 
-# Responses file
-RESPONSES_FILE = PROJECT_DIR / "model_responses.json"
-
-# Setup log directory/files
-LOG_DIR = PROJECT_DIR / "logs"
-LOG_DIR.mkdir(exist_ok=True, parents=True)
-
-OUT_LOG = LOG_DIR / "model_responses.out"
-ERR_LOG = LOG_DIR / "model_responses.err"
-
-try: # remove log files if they already exist
-    OUT_LOG.unlink()
-    ERR_LOG.unlink()
-except FileNotFoundError:
-    pass
-
-# Lock to prevent interleaving prints
-out_lock, err_lock = Lock(), Lock()
-def safe_out_log(*args: Any, **kwargs: Any) -> None:
-    with out_lock, open(OUT_LOG, "a") as out: # to stdout and out log
-        print(*args, **kwargs)
+def safe_out_log(method: str, *args: Any, **kwargs: Any) -> None:
+    with OUT_LOCKS[method], open(OUT_LOG_FILES[method], "a") as out: # to stdout and out_log
+        print(f"[{method.upper()}]", *args, **kwargs)
         print(*args, **kwargs, file=out)
 
-def safe_err_log(*args: Any, **kwargs: Any) -> None:
-    with err_lock, open(ERR_LOG, "a") as err:
+def safe_err_log(method: str, *args: Any, **kwargs: Any) -> None:
+    with ERR_LOCKS[method], open(ERR_LOG_FILES[method], "a") as err:
         print(*args, **kwargs, file=err)
 
-def extract_model_output(idx: int, item: Dict[str, Any]) -> Dict[str, Any]:
+
+def aggregate_benchmarks() -> tuple[list[Any], list[Any]]:
+    ids, aggregate_prompts = [], []
+    for method, prompts in DATASETS.items():
+        aggregate_prompts.extend(prompts)
+        # (prompt index (1-indexed) in resp. dataset, method string (e.g. "base", "scope", "cot", etc.))
+        ids.extend(zip(range(1, N_PROMPTS+1), [method] * N_PROMPTS))
+    return ids, aggregate_prompts
+
+
+def extract_model_output(id: tuple[int, str], item: dict[str, Any]) -> tuple[str, dict[str, Any]]:
+    idx, method = id
+
     messages = [
         {"role": "system", "content": "You are a helpful math assistant."},
         {"role": "user", "content": item["prompt"]}
@@ -72,32 +114,49 @@ def extract_model_output(idx: int, item: Dict[str, Any]) -> Dict[str, Any]:
             model_output = response.choices[0].message.content
             break  # success → exit loop
         except Exception as e:
-            err_msg = f"[ERROR] Prompt {idx+1}: {e}"
+            err_msg = f"[ERROR] Prompt {idx}: {e}"
             retries_left -= 1  # decrease retry counter
             if retries_left == -1:
                 model_output = "<answer> response failed </answer>"
-                safe_err_log(err_msg, f"Out of retries\n", sep="\n")
+                safe_err_log(method, err_msg, f"Out of retries\n", sep="\n")
                 break
             else:
-                safe_err_log(err_msg, f"Retrying... ({retries_left} retries left)\n", sep="\n")
+                safe_err_log(method, err_msg, f"Retrying... ({retries_left} retries left)\n", sep="\n")
     
-    safe_out_log(f"Prompt {idx+1}:", model_output, sep="\n", end="\n\n")
+    safe_out_log(method, f"Prompt {idx}:", model_output, sep="\n", end="\n\n")
 
-    return {
-        # compile simplified result entry
-        "category": item["category"],
-        "question": item["question"],
-        "answer": item["answer"],
-        "model_output": model_output
-    }
+    return (
+        method,
+        {
+            # simplified result entry
+            "category": item["category"],
+            "question": item["question"],
+            "answer": item["answer"],
+            "model_output": model_output
+        }
+    )
 
-with ThreadPoolExecutor(max_workers=N_WORKERS) as executor:
-    results = executor.map(extract_model_output, range(N_PROMPTS), final_benchmark)
 
-# Save to new JSON file
-with open(RESPONSES_FILE, "w") as f:
-    json.dump(list(results), f, indent=2)
+def group_results_by_method(aggregate_results: list[tuple[str, dict[str, Any]]]) -> dict[str, list[dict[str, Any]]]:
+    grouped_results = defaultdict(list)
+    for method, result_entry in aggregate_results:
+        grouped_results[method].append(result_entry)
+    return grouped_results
 
-print(f"Saved question, answer, and model_output to \"{os.path.relpath(RESPONSES_FILE, PROJECT_DIR)}\"")
-print(f"Saved output messages to \"{os.path.relpath(OUT_LOG, PROJECT_DIR)}\"")
-print(f"Saved error messages to \"{os.path.relpath(ERR_LOG, PROJECT_DIR)}\"")
+
+def compute_and_save_results() -> None:
+    ids, prompts = aggregate_benchmarks()
+    with ThreadPoolExecutor(max_workers=N_WORKERS) as executor:
+        aggregate_results = executor.map(extract_model_output, ids, prompts)
+    grouped_results = group_results_by_method(aggregate_results)
+    for method, result_entries in grouped_results.items():
+        with open(PROJECT_DIR / f"{method}_model_responses.json", "w") as f:
+            json.dump(result_entries, f, indent=2)
+
+
+compute_and_save_results()
+
+
+print(f"Saved questions, answers, and model outputs to \"{{method_name}}_model_responses.json\"")
+print(f"Saved output messages to \"{{method_name}}_model_responses.out\"")
+print(f"Saved error messages to \"{{method_name}}_model_responses.err\"")
